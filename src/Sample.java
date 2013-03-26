39073428e0da47cfebfe7e583c783eb78c44ddb4
Kevin Tonon <kevin@betweenconcepts.com>
2013-03-25 20:40:03 -0400

    Fixed keeps

--- src/Sample.java
+++ src/Sample.java
@@ -1,14 +1,20 @@
 public class Sample {
     
     void foo(int arg1) {
-    // keep: foo {
-    // keep: }
+// keep: foo {
+        // While the interface in this example is generated,
+        // the method bodies are preserved between multiple invocations
+        // of the generator.
+    
+        // It doesn't have to be done this way, the method bodies can be
+        // generated aswell, all depends on what your json data encodes
+// keep: }
     }
     
     int bar() {
-    // keep: bar {
+// keep: bar {
         return 1;
-    // keep: }
+// keep: }
     }
     
 }
